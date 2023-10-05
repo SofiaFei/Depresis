eeb95e473d771f894bbb6fe13c38cb47c3f59efa
SofiaFei <Feisofiafei@gmail.com>
2023-10-05 09:16:19 +0700

    style: rapikan folder

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,24 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import DiagnosaDepresi from './components/DiagnosaDepresi'
-import Home from './components/Home'
+import {
+  Navbar,
+  Home,
+  Diagnosa,
+  Footer
+} from "./components";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
-        <Route path='/' exact element={<Home/>}/>
-        <Route path='/Diagnosa' exact element={<DiagnosaDepresi/>}/>
+        <Route path='/' exact element={<Home />} />
+        <Route path='/Diagnosa' element={<Diagnosa />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Navbar from './Navbar'
+import Home from './Home/Home'
+import Diagnosa from './Diagnosa/Diagnosa'
+import Footer from './Footer'
+
+export {Navbar, Home, Diagnosa, Footer}
